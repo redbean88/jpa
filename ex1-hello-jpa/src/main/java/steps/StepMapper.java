d20f3228fd31e8cc4f225631c33d1b083152c819
redbean88 <doctortor88@gmail.com>
2022-01-11 23:30:31 +0900

    update

--- ex1-hello-jpa/src/main/java/steps/StepMapper.java
+++ ex1-hello-jpa/src/main/java/steps/StepMapper.java
@@ -8,10 +8,16 @@ public class StepMapper {
 
     static {
         steps.put("step00" , new Step01());
-        steps.put("step01" , new Step07());
-        steps.put("step02" , new Step08());
-        steps.put("step03" , new Step09());
-        steps.put("step04" , new Step10());
+        steps.put("step01" , new Step01());
+        steps.put("step02" , new Step02());
+        steps.put("step03" , new Step03());
+        steps.put("step04" , new Step04());
+        steps.put("step05" , new Step05());
+        steps.put("step06" , new Step06());
+        steps.put("step07" , new Step07());
+        steps.put("step08" , new Step08());
+        steps.put("step09" , new Step09());
+        steps.put("step10" , new Step10());
     }
 
     public static Step getStep(String target){
