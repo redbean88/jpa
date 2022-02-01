18581a51a9be8bab53fe6a8794884a2698e42517
redbean88 <doctortor88@gmail.com>
2022-02-02 03:15:37 +0900

    회원도메인 개발

---
+++ jpashop/src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,50 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+//    @Rollback(value = false)
+    public void 회원가입(){
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void  중복회원가입_예외(){
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        memberService.join(member2);
+
+        fail("예외가 발행해야 한다.");
+    }
+
+}
