0ff723d70d455401fc0ebe43f4f883ec4eed74c1
redbean88 <doctortor88@gmail.com>
2022-04-19 23:56:16 +0900

    섹션 6 완

---
+++ data-jap/src/main/java/study/datajpa/domain/Item.java
@@ -0,0 +1,32 @@
+package study.datajpa.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+
+    @Id
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @Override
+    public boolean isNew() {
+        return createDate == null;
+    }
+}
